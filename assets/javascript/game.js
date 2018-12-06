e47b664ba3d1ffd83b6ca525344cbec262a3204f
bphan <btp589@gmail.com>
2018-12-05 17:02:24 -0800

    update

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,69 +1,71 @@
 var wins = 0;
 var losses = 0;
-var guessesLeft = 0;
+var guessesLeft = 9;
 var currentGuesses = [];
 
-// Creates an array that lists out all of the options (Rock, Paper, or Scissors).
+// Computer picks a random letter from the alphabet
 function randomLetter() {
   var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", 
                 "j", "k", "l", "m", "n", "o", "p", "q", "r", 
                 "s", "t", "u", "v", "w", "x", "y", "z"];
 
   var randLetter = alphabet[Math.floor(Math.random() * alphabet.length)];
-  console.log(randLetter)
-  //return randomletter;
-};
+
+  return randLetter;
+}
+
+// Resets guessesLeft
+function resetGuesses() {
+    var guessesLeftDiv = document.getElementById('guessesLeft');
+    guessesLeftDiv.textContent = 0;
+
+    return guessesLeftDiv;
+  }
+
 
 
 // This function is run whenever the user presses a key.
 document.onkeyup = function(event) {
+    var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", 
+                    "j", "k", "l", "m", "n", "o", "p", "q", "r", 
+                    "s", "t", "u", "v", "w", "x", "y", "z"];
 
     // Determines which key was pressed.
-    var userGuess = event.key;
-    if(validChoices.indexOf(userGuess) < 0){
-        //alert("You've entered the wrong letter! Only r, p, s allowed.")
+    var userGuess = event.key.toLowerCase();
+    var computerGuess = randomLetter();
+    console.log("Computer guesses: " + computerGuess);
+
+    // Checks if user clicks on any keys that are not alpha, then throw an alert.
+    if (alphabet.indexOf(userGuess) < 0){
+        //console.log("---You've entered the wrong key! Only letters in the alphabet are allowed.---")
         return false;
     }
 
-    // Randomly chooses a choice from the options array. This is the Computer's guess.
-    var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
-
-    //TIE condition and update ties count on DOM
+    // WIN condition and update wins count
     if (userGuess == computerGuess) {
-
-    ties++;
-
-    var tiesDiv = document.getElementById('ties'); //created new tiesDiv to hold ties in html
-    tiesDiv.textContent = ties; //updates tiesDiv with the current value for ties
-    }
-
-    //WIN condition and update wins count on DOM
-    else if ( (userGuess == "r" && computerGuess == "s") || 
-            (userGuess == "s" && computerGuess == "p") || 
-            (userGuess == "p" && computerGuess == "r") ) {
-
     wins++;
 
-    var winsDiv = document.getElementById('wins'); //created new winsDiv to hold ties in html
-    winsDiv.textContent = wins; //updates winsDiv with the current value for ties
+    console.log("You guessed: " + userGuess);
+    console.log("Computer guessed: " + computerGuess);
+    alert("You win 1 point!");
+
+    var winsDiv = document.getElementById('wins'); //created new tiesDiv to hold ties in html
+    winsDiv.textContent = wins; //updates tiesDiv with the current value for ties
     }
 
-    //LOSE condition and update losses count on DOM
-    else if ( (userGuess == "r" && computerGuess == "p") || 
-            (userGuess == "s" && computerGuess == "r") || 
-            (userGuess == "p" && computerGuess == "s") ) {
-                
-    losses++;
 
-    var lossesDiv = document.getElementById('losses'); //created new lossesDiv to hold ties in html
-    lossesDiv.textContent = losses; //updates lossesDiv with the current value for ties
+    // WRONG condition that decrements guessesLeft count
+    else if (userGuess != computerGuess) {
+    guessesLeft--;
+
+    var guessesLeftDiv = document.getElementById('guessesLeft'); //created new lossesDiv to hold ties in html
+    guessesLeftDiv.textContent = guessesLeft; //updates lossesDiv with the current value for ties
+
+        if (guessesLeft == 0) {
+            
+        }
     }
 
     
-    // Alerts the key the user pressed (userGuess).
-    console.log("User guess: " + userGuess);
-
-    // Alerts the Computer's guess.
-    console.log("Computer guess: " + computerGuess);
 
 };
